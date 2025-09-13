331abf348f4dd2a9cf94a1b3a481db6961cc7c8c
willn <welfordwillian@gmail.com>
2025-09-13 03:00:21 -0500

    Agregado ejercicio.java para la tarea

---
+++ ejercicio1.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = 10 ; 
+        int num2 = 10 ; 
+        int suma = num1+num2 ; 
+    }
+}
